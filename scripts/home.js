27c612035beafa54e2bb2eb6742b20dbd1edda96
kaziontor7 <kaziontor7@gmail.com>
2026-03-08 17:13:31 +0600

    cards done

---
+++ scripts/home.js
@@ -0,0 +1,84 @@
+
+const fetchAll = ()=>{
+    const url= 'https://phi-lab-server.vercel.app/api/v1/lab/issues'
+    fetch(url)
+    .then(res => res.json())
+    .then(data=> {
+        console.log(data.data)
+        showAll(data.data)})
+}
+
+fetchAll();
+
+// assignee
+// : 
+// "jane_smith"
+// author
+// : 
+// "john_doe"
+// createdAt
+// : 
+// "2024-01-15T10:30:00Z"
+// description
+// : 
+// "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior."
+// id
+// : 
+// 1
+// labels
+// : 
+// (2) ['bug', 'help wanted']
+// priority
+// : 
+// "high"
+// status
+// : 
+// "open"
+// title
+// : 
+// "Fix navigation menu on mobile devices"
+// updatedAt
+// : 
+// "2024-01-15T10:30:00Z"
+
+
+const showAll = (datas) => {
+  const cardContainer = document.getElementById('card-container') ;
+//   cardContainer.innerHTML="";
+  datas.forEach(info => {
+    const createE = document.createElement('div');
+    createE.innerHTML=`
+     <div class="py-4 px-4 bg-white rounded-b-lg drop-shadow-sm border-t-5  rounded-t-sm h-full ${info.status == 'open'? 'border-green-600' : 'border-purple-600'}">
+        <div class="flex justify-between pb-4 ">
+            <div>
+                ${info.status == 'open'? `<img src="./assets/Open-Status.png" alt=""></img>` : `<img src="./assets/closed.png" alt=""></img>`}
+            </div>
+            <div>
+                <p class="w-20 rounded-full py-1 text-[12px] font-medium text-center
+                ${info.priority == 'high' ? 'bg-[#FEECEC]  text-[#EF4444]': '' }
+                ${info.priority == 'medium' ? 'bg-[#FFF6D1] text-[#F59E0B]': '' }   ${info.priority == 'low'? 'text-[#9CA3AF] bg-[#EEEFF2]': '' }">${info.priority.toUpperCase()}</p>
+            </div>
+        </div>
+        <div class="">
+            <h2 class="text-sm font-semibold pb-2">${info.title}</h2>
+            <p class="text-[#64748B] text-[12px]">${info.description}</p>
+        </div>
+        <div class="flex flex-wrap gap-1 pt-3">
+             <p class="bg-[#FEECEC]  text-[#EF4444] px-2 rounded-full py-1 text-[12px] font-medium text-center border border-[#FECACA]"><i class="fa-solid fa-bug"></i> ${info.labels[0].toUpperCase()}</p>
+             ${info.labels[1] ? `
+  <p class="bg-[#FFF8DB] text-[#D97706] rounded-full py-1 text-[12px] font-medium text-center border border-[#FDE68A] px-2">
+    <i class="fa-solid fa-life-ring"></i> ${info.labels[1].toUpperCase()}
+  </p>` 
+: ''}
+             
+        </div>
+        <p class="border-b-2 border-gray-200 py-4"></p>
+        <p class="text-[12px] text-[#64748B] pt-4 pb-2">#${info.id}
+by ${info.author}</p>
+        <p class="text-[12px] text-[#64748B]">${new Date(info.updatedAt).toLocaleDateString()}</p>
+    </div>
+    `
+    cardContainer.appendChild(createE)
+  });
+
+}
