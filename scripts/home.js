1d1975da3a2b2cbacfdbfa67b9925169902552e3
kaziontor7 <kaziontor7@gmail.com>
2026-03-09 17:33:59 +0600

    added search

--- scripts/home.js
+++ scripts/home.js
@@ -132,88 +132,15 @@ allBtn.addEventListener('click', function (){
 openBtn.addEventListener('click', function(){
   openSelected();
   cardContainer.innerHTML='';
-    datas.forEach(info => {
-    if(info.status=='open'){
-    const createE = document.createElement('div');
-    createE.innerHTML=`
-     <div onclick="fetchById(${info.id})" class="cursor-pointer py-4 px-4 bg-white rounded-b-lg drop-shadow-sm border-t-5  rounded-t-sm h-full ${info.status == 'open'? 'border-green-600' : 'border-purple-600'}">
-        <div class="flex justify-between pb-4 ">
-            <div>
-                ${info.status == 'open'? `<img src="./assets/Open-Status.png" alt=""></img>` : `<img src="./assets/closed.png" alt=""></img>`}
-            </div>
-            <div>
-                <p class="w-20 rounded-full py-1 text-[12px] font-medium text-center
-                ${info.priority == 'high' ? 'bg-[#FEECEC]  text-[#EF4444]': '' }
-                ${info.priority == 'medium' ? 'bg-[#FFF6D1] text-[#F59E0B]': '' }   ${info.priority == 'low'? 'text-[#9CA3AF] bg-[#EEEFF2]': '' }">${info.priority.toUpperCase()}</p>
-            </div>
-        </div>
-        <div class="">
-            <h2 class="text-sm font-semibold pb-2">${info.title}</h2>
-            <p class="text-[#64748B] text-[12px]">${info.description}</p>
-        </div>
-        <div class="flex flex-wrap gap-1 pt-3">
-             <p class="bg-[#FEECEC]  text-[#EF4444] px-2 rounded-full py-1 text-[12px] font-medium text-center border border-[#FECACA]"><i class="fa-solid fa-bug"></i> ${info.labels[0].toUpperCase()}</p>
-             ${info.labels[1] ? `
-  <p class="bg-[#FFF8DB] text-[#D97706] rounded-full py-1 text-[12px] font-medium text-center border border-[#FDE68A] px-2">
-    <i class="fa-solid fa-life-ring"></i> ${info.labels[1].toUpperCase()}
-  </p>` 
-: ''}
-             
-        </div>
-        <p class="border-b-2 border-gray-200 py-4"></p>
-        <p class="text-[12px] text-[#64748B] pt-4 pb-2">#${info.id}
-by ${info.author}</p>
-        <p class="text-[12px] text-[#64748B]">${new Date(info.updatedAt).toLocaleDateString()}</p>
-    </div>
-    `
-    cardContainer.appendChild(createE)
-    }
-     totalN();
-  });
+  const openIssue = datas.filter(info=> info.status === 'open')
+  console.log(openIssue);
+  showAll(openIssue);
 })
 closeBtn.addEventListener('click', function(){
   closeSelected();
-  manageSpinner(true);
   cardContainer.innerHTML='';
-    datas.forEach(info => {
-    if(info.status!=='open'){
-    const createE = document.createElement('div');
-    createE.innerHTML=`
-     <div onclick="fetchById(${info.id})" class="cursor-pointer py-4 px-4 bg-white rounded-b-lg drop-shadow-sm border-t-5  rounded-t-sm h-full ${info.status == 'open'? 'border-green-600' : 'border-purple-600'}">
-        <div class="flex justify-between pb-4 ">
-            <div>
-                ${info.status == 'open'? `<img src="./assets/Open-Status.png" alt=""></img>` : `<img src="./assets/closed.png" alt=""></img>`}
-            </div>
-            <div>
-                <p class="w-20 rounded-full py-1 text-[12px] font-medium text-center
-                ${info.priority == 'high' ? 'bg-[#FEECEC]  text-[#EF4444]': '' }
-                ${info.priority == 'medium' ? 'bg-[#FFF6D1] text-[#F59E0B]': '' }   ${info.priority == 'low'? 'text-[#9CA3AF] bg-[#EEEFF2]': '' }">${info.priority.toUpperCase()}</p>
-            </div>
-        </div>
-        <div class="">
-            <h2 class="text-sm font-semibold pb-2">${info.title}</h2>
-            <p class="text-[#64748B] text-[12px]">${info.description}</p>
-        </div>
-        <div class="flex flex-wrap gap-1 pt-3">
-             <p class="bg-[#FEECEC]  text-[#EF4444] px-2 rounded-full py-1 text-[12px] font-medium text-center border border-[#FECACA]"><i class="fa-solid fa-bug"></i> ${info.labels[0].toUpperCase()}</p>
-             ${info.labels[1] ? `
-  <p class="bg-[#FFF8DB] text-[#D97706] rounded-full py-1 text-[12px] font-medium text-center border border-[#FDE68A] px-2">
-    <i class="fa-solid fa-life-ring"></i> ${info.labels[1].toUpperCase()}
-  </p>` 
-: ''}
-             
-        </div>
-        <p class="border-b-2 border-gray-200 py-4"></p>
-        <p class="text-[12px] text-[#64748B] pt-4 pb-2">#${info.id}
-by ${info.author}</p>
-        <p class="text-[12px] text-[#64748B]">${new Date(info.updatedAt).toLocaleDateString()}</p>
-    </div>
-    `
-    cardContainer.appendChild(createE)
-    }
-    manageSpinner(false);
-     totalN();
-  });
+   const closeIssue = datas.filter(info=> info.status !== 'open')
+  showAll(closeIssue);
 })
 }
 
@@ -243,3 +170,21 @@ const total = document.getElementById('total');
 function totalN(){
   total.innerText = cardContainer.children.length;
 }
+
+document.getElementById('search').addEventListener('keyup',function (){
+  allBtn.classList.add('text-[#64748B]')
+  openBtn.classList.remove('text-white', 'bg-[#4A00FF]')
+  closeBtn.classList.remove('text-white', 'bg-[#4A00FF]')
+  allBtn.classList.remove('text-white', 'bg-[#4A00FF]')
+ const value = document.getElementById('search').value.trim().toLowerCase();
+ const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${value}`
+ manageSpinner(true);
+ fetch(url)
+ .then(res => res.json())
+ .then(data =>{
+  console.log(data);
+  showAll(data.data);
+  manageSpinner(false);
+ })
+ 
+})
